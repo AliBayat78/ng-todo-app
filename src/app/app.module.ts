2d36e81850e33a80576127eb8ef65c3439c06938
Ali Bayat <ali.bayat77@yahoo.com>
2023-07-19 16:47:18 +0430

    add todo functionality fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TodosComponent } from './components/todos/todos.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TodosComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -3,6 +3,24 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Todos } from 'src/app/models/todos.model';
 
+const TODOS: Todos[] = [
+  {
+    task: 'asd',
+    done: false,
+    date: Date.now(),
+  },
+  {
+    task: 'asd',
+    done: false,
+    date: Date.now(),
+  },
+  {
+    task: 'asd',
+    done: false,
+    date: Date.now(),
+  },
+];
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -12,23 +30,25 @@ export class TodosComponent {
   modalService = inject(NgbModal);
   formService = inject(FormBuilder);
 
-  todos: Todos[] = [
-    {
-      task: 'asd',
-      done: false,
-      date: Date.now(),
-    },
-    {
-      task: 'asd',
-      done: false,
-      date: Date.now(),
-    },
-    {
-      task: 'asd',
-      done: false,
-      date: Date.now(),
-    },
-  ];
+  page = 1;
+  pageSize = 4;
+  todos: Todos[] = [];
+  collectionSize = TODOS.length;
+
+  constructor() {
+    this.collectionSize = TODOS.length;
+    this.refreshTodos();
+  }
+
+  refreshTodos() {
+    this.todos = TODOS.map((todo, i) => ({
+      id: i + 1,
+      ...todo,
+    })).slice(
+      (this.page - 1) * this.pageSize,
+      (this.page - 1) * this.pageSize + this.pageSize
+    );
+  }
 
   openVerticallyCentered(content: TemplateRef<any>) {
     this.modalService.open(content, { centered: true });
@@ -44,5 +64,20 @@ export class TodosComponent {
     return this.todosForm.get('task');
   }
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.todosForm.valid) {
+      const newTodo: Todos = {
+        task: this.todosForm.value.task!,
+        done: this.todosForm.value.done || false,
+        date: Date.now(),
+      };
+
+      TODOS.push(newTodo);
+      this.todosForm.reset();
+
+      this.collectionSize = TODOS.length;
+
+      this.refreshTodos();
+    }
+  }
 }
